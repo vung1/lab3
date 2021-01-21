019041bb03800aa2ec8f1bd067c876d4e09ecf99
Victor Ung <vung@ucsd.edu>
2021-01-20 21:43:11 -0800

    finished lab

--- static/js/introHCI.js
+++ static/js/introHCI.js
@@ -1,18 +1,39 @@
-'use strict';
+"use strict";
 
 // Call this function when the page loads (the "ready" event)
-$(document).ready(function() {
-	initializePage();
-})
+$(document).ready(function () {
+    initializePage();
+});
 
 /*
  * Function that is called when the document is ready.
  */
 function initializePage() {
-	$("#testjs").click(function(e) {
-		$('.jumbotron h1').text("Javascript is connected");
-	});
+    $("#testjs").click(function (e) {
+        $(".jumbotron h1").text("Javascript is connected");
+        $("#testjs").text("Please wait...");
+        // $(".jumbotron p").addClass("active");
+        $(".jumbotron p").toggleClass("active");
+    });
 
-	// Add any additional listeners here
-	// example: $("#div-id").click(functionToCall);
+    // Add any additional listeners here
+    // example: $("#div-id").click(functionToCall);
+    $(".project").click(projectClick);
+}
+
+function projectClick(e) {
+    e.preventDefault();
+
+    var containingProject = $(this).closest(".project");
+    var description = $(containingProject).find(".project-description");
+    if (description.length == 0) {
+        $(containingProject).append(
+            "<div class='project-description'><p>Description of the project.</p></div>"
+        );
+    } else {
+        description.toggle();
+        // description.html(
+        //     "<p>Stop clicking on me! You just did it at " + new Date() + "</p>"
+        // );
+    }
 }
